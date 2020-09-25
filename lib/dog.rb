f067a5256461151da8594bcc1fa94b8c9b9463d7
Pbsmith82 <pbsmith82@gmail.com>
2020-09-25 07:56:11 -0400

    Updated Comments to Suggest Code

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 require"pry"
-# Add your code here
+
 class Dog
-    attr_accessor :name, :print_all
+    attr_accessor :name, :print_all # :print_all not needed
 
     @@all = []
-    @@name = []
+    @@name = [] # Not Needed with Map Code Below
 
     def save
             @@all << self
@@ -12,7 +12,7 @@ class Dog
 
     def initialize(name)
             @name = name
-            @@name << @name
+            @@name << @name # Not Needed with Map Code Below
             save
     end
 
@@ -20,13 +20,13 @@ class Dog
             @@all
     end
 
-    def self.clear_all
-            @@all = []
-            @@name = []
+    def self.clear_all  # def self.clear_all
+            @@all = []  #       @@all.clear
+            @@name = [] # end
     end
 
     def self.print_all
-            puts @@name
+            puts @@name # puts @@all.map{ |dog| dog.name }
     end
 
 
